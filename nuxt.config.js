e25dc5241c471c6da1dcbe618eacb8f3123dd984
kinhang <kinhang@trip.io>
2021-11-04 09:56:32 +0800

    first version. place holder

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,30 +1,46 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+    // Target: https://go.nuxtjs.dev/config-target
+    target: 'static',
 
-  // Global App headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'My Nuxt Application',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'This is an awesome description of my Nuxt app',
-      },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+    // Global App headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'My Nuxt Application',
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'This is an awesome description of my Nuxt app',
+            },
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+        ],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: ['@nuxtjs/tailwindcss'],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+            {rel: 'preload', href: 'https://awesome11ty.ttntm.me/css/page.css'},
+            {rel: 'preconnect', href: 'https://unpkg.com/popper.js@1'},
+            {rel: 'preconnect', href: 'https://unpkg.com/tippy.js@4'},
+            {rel: 'stylesheet', href: 'https://awesome11ty.ttntm.me/css/page.css'},
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: []
+
+        ],
+        script: [
+            {src: 'https://unpkg.com/popper.js@1'},
+            {src: 'https://unpkg.com/tippy.js@4'},
+        ],
+        __dangerouslyDisableSanitizers: ['script', 'innerHTML'],
+    },
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: ['@nuxtjs/tailwindcss'],
+
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: []
 };
