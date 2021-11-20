0f2bf723bb3e54c3fcf41a5faa3b2d6df75db7e7
kinhang <kinhang@trip.io>
2021-11-20 11:07:48 +0800

    navbar as a component

--- pages/wallet/create.vue
+++ pages/wallet/create.vue
@@ -1,31 +1,6 @@
 <template>
   <div><!---->
-    <nav
-        class="flex flex-col sm:flex-row w-full justify-between items-center px-4 sm:px-6 py-1 bg-white sm:bg-transparent shadow sm:shadow-none">
-      <div
-          class="w-full sm:w-auto self-start sm:self-center flex flex-row sm:flex-none flex-no-wrap justify-between items-center">
-        <a href="/" class="no-underline"><img src="/img/logo.png" alt="AWE.SOME Logo" class="h-12 py-1"></a>
-        <button id="menuBtn" class="hamburger block sm:hidden focus:outline-none" type="button"
-
-                aria-label="Show Menu"><span class="hamburger__top-bun"></span><span
-            class="hamburger__bottom-bun"></span></button>
-      </div>
-      <div id="menu"
-           class="w-full sm:w-auto self-end sm:self-center hidden sm:flex flex-col sm:flex-row items-center h-full py-1 pb-4 sm:py-0 sm:pb-0">
-        <!--<a class="text-gray-800 font-bold hover:text-purple-600 text-lg text-center w-full no-underline sm:w-auto sm:pr-4 py-2 sm:py-1"-->
-        <!--href="#about">About</a> <a-->
-        <!--class="text-gray-800 font-bold hover:text-purple-600 text-lg text-center w-full no-underline sm:w-auto sm:px-4 py-2 sm:py-1"-->
-        <!--href="#features">Features</a> -->
-        <a id="follow" href="https://twitter.com/authmint"
-           class="hidden sm:inline-block text-gray-800 font-bold hover:text-purple-600 text-lg sm:w-auto sm:px-4 sm:py-1 cursor-pointer"
-           tabindex="0">Twitter</a>
-
-        <!--<a-->
-        <!--class="text-gray-800 border border-indigo-800 text-lg bg-gray-100 rounded-full w-auto no-underline text-center sm:text-left hover:border-purple-600 hover:text-purple-600 hover:bg-white hover:shadow-md px-6 py-1 my-2 sm:my-0 sm:ml-4"-->
-        <!--href="#register">Register Now</a>-->
-
-      </div>
-    </nav>
+    <Navbar/>
     <div class="pt-2 m-10 ">
 
       <ul class="w-full steps">
@@ -175,10 +150,11 @@
               </label>
               <br>
               <div class="m-5 text-right">
-                <a href="/wallet">
-                  <button class="btn btn-secondary btn-outline" v-on:click="step=2">Back to wallet
+                <router-link to="/wallet">
+
+                <button class="btn btn-secondary btn-outline" v-on:click="step=2">Back to wallet
                   </button>
-                </a>
+                </router-link>
 
                 <button class="btn btn-secondary" v-on:click="mint">View</button>
 
@@ -198,9 +174,11 @@
 import {ethers} from "ethers";
 import amoAddress from '~/static/contracts/address-Origin.json'
 import amoABI from '~/static/contracts/Origin.json'
+import Navbar from "../../components/Navbar";
 
 
 export default {
+  components: {Navbar},
   data() {
     return {
       amoAddress: amoAddress.contract,
--- pages/wallet/index.vue
+++ pages/wallet/index.vue
@@ -1,43 +1,18 @@
 <template>
   <div><!---->
-    <nav
-        class="flex flex-col sm:flex-row w-full justify-between items-center px-4 sm:px-6 py-1 bg-white sm:bg-transparent shadow sm:shadow-none">
-      <div
-          class="w-full sm:w-auto self-start sm:self-center flex flex-row sm:flex-none flex-no-wrap justify-between items-center">
-        <a href="/" class="no-underline"><img src="/img/logo.png" alt="AWE.SOME Logo" class="h-12 py-1"></a>
-        <button id="menuBtn" class="hamburger block sm:hidden focus:outline-none" type="button"
-                v-on:click="navToggle"
-                aria-label="Show Menu"><span class="hamburger__top-bun"></span><span
-            class="hamburger__bottom-bun"></span></button>
-      </div>
-      <div id="menu"
-           class="w-full sm:w-auto self-end sm:self-center hidden sm:flex flex-col sm:flex-row items-center h-full py-1 pb-4 sm:py-0 sm:pb-0">
-        <!--<a class="text-gray-800 font-bold hover:text-purple-600 text-lg text-center w-full no-underline sm:w-auto sm:pr-4 py-2 sm:py-1"-->
-        <!--href="#about">About</a> <a-->
-        <!--class="text-gray-800 font-bold hover:text-purple-600 text-lg text-center w-full no-underline sm:w-auto sm:px-4 py-2 sm:py-1"-->
-        <!--href="#features">Features</a> -->
-        <a id="follow" href="https://twitter.com/authmint"
-           class="hidden sm:inline-block text-gray-800 font-bold hover:text-purple-600 text-lg sm:w-auto sm:px-4 sm:py-1 cursor-pointer"
-           tabindex="0">Twitter</a>
-
-        <!--<a-->
-        <!--class="text-gray-800 border border-indigo-800 text-lg bg-gray-100 rounded-full w-auto no-underline text-center sm:text-left hover:border-purple-600 hover:text-purple-600 hover:bg-white hover:shadow-md px-6 py-1 my-2 sm:my-0 sm:ml-4"-->
-        <!--href="#register">Register Now</a>-->
-
-      </div>
-    </nav>
+    <Navbar/>
     <div class="pt-2 m-10">
       <div class="w-full flex justify-end	">
-        <a href="/wallet/create">
+        <router-link to="/wallet/create">
           <button class="btn btn-primary" v-if="!loading">
             Create NFT
           </button>
-        </a>
+        </router-link>
       </div>
       <button class="btn btn-primary" v-if="loading">
         Loading...
       </button>
-      <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6" v-if="!loading">
+      <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6 pt-4" v-if="!loading">
 
         <div class="card bordered" v-for="item in items" :key="item.image">
           <div class="w-full aspect-w-1 aspect-h-1 "><img v-bind:src="getIPFSlink(item.imageIpfs)"
@@ -65,9 +40,11 @@ import marketABI from '~/static/contracts/Market.json'
 import marketAddress from '~/static/contracts/address-Market.json'
 import amoAddress from '~/static/contracts/address-Origin.json'
 import amoABI from '~/static/contracts/Origin.json'
+import Navbar from "../../components/Navbar";
 
 
 export default {
+  components: {Navbar},
   data() {
     return {
       amoAddress: amoAddress.contract,
